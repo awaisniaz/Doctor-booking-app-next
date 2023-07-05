303767a36654387bcbc42d5bb006f1c87bf193df
awais.niaz@mwanmobile.org <awais.niaz@mwanmobile.org>
2023-07-05 09:42:42 +0500

    add what you want section

--- pages/home/index.js
+++ pages/home/index.js
@@ -3,12 +3,14 @@ import Loader from '../../components/loader'
 import Header from '../../components/Header.js'
 import Banner from '../../components/Banner'
 import SearchBar from '../../components/SearchBar'
+import LookFor from '../../components/WhatYou'
 const Home = () => {
   return (
     <>
       <Header />
       <Banner />
       <SearchBar />
+      <LookFor />
     </>
   )
 }
